a1dad26d2fcce84a000884bc36532ea8cd0e69c0
Ivan Reshetnikov <resivalex@gmail.com>
2021-11-05 22:03:36 +0300

    Handle "/start"

--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,19 @@
-print('Hi!')
+import json
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+
+
+with open('config.json') as fin:
+    config = json.loads(fin.read())
+
+updater = Updater(token=config['token'], use_context=True)
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
