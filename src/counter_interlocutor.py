f4f49caeddd872c9902636c47b6f4edab250ddfc
Ivan Reshetnikov <resivalex@gmail.com>
2021-11-06 18:40:54 +0300

    Refactor CounterInterlocutor

--- src/counter_interlocutor.py
+++ src/counter_interlocutor.py
@@ -9,46 +9,29 @@ class CouterInterlocutor(AbstractInterlocutor):
 
     def greet(self, chat_id: int):
         self.__ensure_sql_table_presence()
-        if self.__db.fetch_all(f'''
-                SELECT COUNT(*)
-                FROM counts
-                WHERE chat_id = {chat_id}''')[0][0] == 0:
-            self.__db.execute(f'''
-                INSERT INTO counts
-                (chat_id, counter)
-                VALUES
-                ({chat_id}, 0)
-            ''')
+        if self.__is_new_chat(chat_id):
+            self.__put_initial_record(chat_id)
             return 'Welcome!'
         else:
             return 'Welcome, again!'
 
     def say(self, chat_id: int, message: str):
         self.__ensure_sql_table_presence()
-        if self.__db.fetch_all(f'''
-                SELECT COUNT(*)
-                FROM counts
-                WHERE chat_id = {chat_id}''')[0][0] == 0:
+        if self.__is_new_chat(chat_id):
             counter = 1
-            self.__db.execute(f'''
-                INSERT INTO counts
-                (chat_id, counter)
-                VALUES
-                ({chat_id}, {counter})
-            ''')
+            self.__put_initial_record(chat_id)
+            self.__update_chat_counter(chat_id, counter)
         else:
-            counter = self.__db.fetch_all(f'''
-                SELECT counter
-                FROM counts
-                WHERE chat_id = {chat_id}
-            ''')[0][0]
+            counter = self.__get_chat_counter(chat_id)
             counter += 1
-            self.__db.execute(f'''
-                UPDATE counts
-                SET counter = {counter}
-                WHERE chat_id = {chat_id}
-            ''')
+            self.__update_chat_counter(chat_id, counter)
         return f'You wrote messages {counter} times'
+
+    def __is_new_chat(self, chat_id):
+        return self.__db.fetch_all(f'''
+            SELECT COUNT(*)
+            FROM counts
+            WHERE chat_id = {chat_id}''')[0][0] == 0
 
     def __ensure_sql_table_presence(self):
         self.__db.execute('''
@@ -57,3 +40,25 @@ class CouterInterlocutor(AbstractInterlocutor):
                 counter INTEGER
             )
         ''')
+
+    def __put_initial_record(self, chat_id):
+        self.__db.execute(f'''
+            INSERT INTO counts
+            (chat_id, counter)
+            VALUES
+            ({chat_id}, 0)
+        ''')
+
+    def __update_chat_counter(self, chat_id, counter):
+        self.__db.execute(f'''
+            UPDATE counts
+            SET counter = {counter}
+            WHERE chat_id = {chat_id}
+        ''')
+
+    def __get_chat_counter(self, chat_id):
+        return self.__db.fetch_all(f'''
+            SELECT counter
+            FROM counts
+            WHERE chat_id = {chat_id}
+        ''')[0][0]
